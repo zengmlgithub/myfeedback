23781d850b065565a51206bd003b683c24af23e5
zmlsc888_home <zmlsc888@126.com>
2019-06-16 06:24:06 +0800

    反馈我的增加上拉下拉功能

--- app/src/main/java/com/sanyedu/myfeedback/fragment/BaseFeedbackMyFragment.java
+++ app/src/main/java/com/sanyedu/myfeedback/fragment/BaseFeedbackMyFragment.java
@@ -3,12 +3,17 @@ package com.sanyedu.myfeedback.fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.sanyedu.myfeedback.R;
+import com.sanyedu.myfeedback.activity.ModifyDetailActivity;
 import com.sanyedu.myfeedback.adapter.NeedModifyAdapter;
 import com.sanyedu.myfeedback.base.BaseFragment;
+import com.sanyedu.myfeedback.listener.EndlessRecyclerOnScrollListener;
 import com.sanyedu.myfeedback.log.SanyLogs;
 import com.sanyedu.myfeedback.model.Records;
 import com.sanyedu.myfeedback.model.TeacherBean;
@@ -16,29 +21,43 @@ import com.sanyedu.myfeedback.mvpimpl.MyFeedbackFragment.FeedbackMyFragmentPrese
 import com.sanyedu.myfeedback.mvpimpl.MyFeedbackFragment.MyFeedbackFragmentContacts;
 import com.sanyedu.myfeedback.share.SpHelper;
 import com.sanyedu.myfeedback.utils.ConstantUtil;
+import com.sanyedu.myfeedback.utils.StartUtils;
+import com.sanyedu.myfeedback.wrapper.LoadMoreWrapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  *  反馈我的
  */
 public class BaseFeedbackMyFragment extends BaseFragment<FeedbackMyFragmentPresenter> implements MyFeedbackFragmentContacts.IMyFeedbackFragmentUI{
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+    @BindView(R.id.feedback_rl)
+    RecyclerView recyclerView;
+
+    @BindView(R.id.pulldown_srl)
+    SwipeRefreshLayout swipeRefreshLayout;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    private RecyclerView listView;
-    private NeedModifyAdapter adapter;
+//    private RecyclerView listView;
+    private NeedModifyAdapter recordAdapter;
+    private List<Records> currList = new ArrayList<>();
+    private final int PAGE_COUNT = 4;
+    private LoadMoreWrapper loadMoreWrapper;
+    private int currentPage = 1;
+    //最大页面数,初始化为1
+    private int  totalSize = 1;
+
 
     public BaseFeedbackMyFragment() {
-        // Required empty public constructor
+
     }
 
     public static BaseFeedbackMyFragment newInstance(String param1, String param2) {
@@ -61,18 +80,15 @@ public class BaseFeedbackMyFragment extends BaseFragment<FeedbackMyFragmentPrese
 
     @Override
     protected void init(View view) {
-        listView = view.findViewById(R.id.wait_el);
-        adapter = new NeedModifyAdapter(getContext());
-        listView.setHasFixedSize(true);
-        listView.setAdapter(adapter);
-        LinearLayoutManager layout = new LinearLayoutManager(getContext());
-        listView.setLayoutManager(layout);
-
-        setData();
+        ButterKnife.bind(this,view);
+        initParams();
+        initRefreshLayout();
+        initRecycleView();
+        setListener();
+        getFirstPageData();
     }
 
-    private void setData() {
-//        getPresenter().getRecords("1","10","1");
+    private void initParams() {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -80,16 +96,61 @@ public class BaseFeedbackMyFragment extends BaseFragment<FeedbackMyFragmentPrese
             mParam1 = ConstantUtil.FK_STATE_SUBMITED;
             mParam2 = null;
         }
-
-        //TODO:
-        TeacherBean teacherBean = SpHelper.getObj(ConstantUtil.USERINFO);
-        if(teacherBean != null){
-            SanyLogs.i("get id:" + teacherBean.getId() + ",type:" + mParam1);
-            getPresenter().getFeedbacks("1","10",teacherBean.getId(),mParam1);
-        }
-
     }
 
+    private void setListener() {
+        // 设置下拉刷新
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                currList.clear();
+                currentPage = 1;
+                getDataFromServer();
+
+            }
+        });
+
+        // 设置加载更多监听
+        recyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener() {
+            @Override
+            public void onLoadMore() {
+                SanyLogs.i("onLoadMore~~~~");
+                loadMoreWrapper.setLoadState(loadMoreWrapper.LOADING);
+                if ( currentPage < totalSize) {
+                    addMoreRecords();
+                } else {
+                    // 显示加载到底的提示
+                    loadMoreWrapper.setLoadState(loadMoreWrapper.LOADING_END);
+                }
+            }
+        });
+
+        recordAdapter.setItemClickListener(new NeedModifyAdapter.OnItemClickListener(){
+            @Override
+            public void onClick(View view, int position, String id) {
+                SanyLogs.i("noticeclick~~~~position:" +position + ",id:" + id);
+                StartUtils.startActivity(getContext(), ModifyDetailActivity.class,id);
+            }
+        });
+    }
+
+    private void addMoreRecords() {
+        currentPage ++;
+        getDataFromServer();
+    }
+
+    private void initRecycleView() {
+        recordAdapter = new NeedModifyAdapter(getContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(recordAdapter);
+        LinearLayoutManager layout = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layout);
+    }
+
+    private void initRefreshLayout() {
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_red_light,
+                android.R.color.holo_orange_light, android.R.color.holo_green_light);
+    }
 
     @Override
     public FeedbackMyFragmentPresenter onBindPresenter() {
@@ -132,12 +193,26 @@ public class BaseFeedbackMyFragment extends BaseFragment<FeedbackMyFragmentPrese
 
     @Override
     public void setFeebacks(List<Records> recordsList) {
-        adapter.setRecordsList(recordsList);
+        recordAdapter.setRecordsList(recordsList);
     }
 
 
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void getDataFromServer() {
+        TeacherBean teacherBean = SpHelper.getObj(ConstantUtil.USERINFO);
+        if(teacherBean != null){
+            SanyLogs.i("get id:" + teacherBean.getId() + ",type:" + mParam1);
+            getPresenter().getFeedbacks(currentPage + "",PAGE_COUNT + "",teacherBean.getId(),mParam1);
+        }
+    }
+
+    private void getFirstPageData() {
+        currentPage = 1;
+        currList.clear();
+        getDataFromServer();
     }
 }
